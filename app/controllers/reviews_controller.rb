26fd2cf2136be215504e30f9398d94ba3ada24c0
Bobby Hinton <bob.g.hint@gmail.com>
2023-02-17 20:10:18 -0600

    commit tree 69e032c40005ecee1e39e388da7b6a36f9825fb0

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,21 @@ class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    reviews = Review.all
+    # here we are using our nested route.. if a dog house ID is passed in
+    # we want to only get the reviews for THAT dog house and not ALL reviews
+
+
+    # so do do this, we use some logic which first checks if a dog house ID
+    # is present..if it is.. then we only find reviews for a certain dog
+    # house.. if not we want ALL the reviews 
+    
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+
     render json: reviews, include: :dog_house
   end
 
